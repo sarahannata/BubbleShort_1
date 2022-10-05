76e78f2f6912df665fedfb4bfa68240e6e14f82c
sarahannata <sarahannata@gmail.com>
2022-10-05 17:27:22 +0700

    Deklarasi array int dengan ukuran 20. Deklarasi variable int untuk menyimpan banyaknya data pada array. Menerima angka untuk menentukan banyaknya data yang disimpan pada array.

--- BubbleShort_1/Program.cs
+++ BubbleShort_1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace BubbleShort
+{
+    class Program
+    {
+        //Deklarasi array int dengan ukuran 20
+        private int[] a = new int[20];
+        //Deklarasi variable int untuk menyimpan banyaknya data pada array
+        private int n;
+        public void read()
+        {
+            //Menerima angka untuk menentukan banyaknya data yang disimpan pada array
+            while (true)
+            {
+                Console.Write("Masukkan banyaknya elemen pada array : ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                {
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 20 element.\n");
+                }
+                Console.WriteLine("");
+                Console.WriteLine("----------");
+            }
+        }
+    }
+}

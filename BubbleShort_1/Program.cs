0e9b4b5906490bf40d4fd04b89dec41e94b34a28
sarahannata <sarahannata@gmail.com>
2022-10-05 17:32:45 +0700

    Pengguna memasukkan elemen pada array.

--- BubbleShort_1/Program.cs
+++ BubbleShort_1/Program.cs
@@ -21,7 +21,21 @@
                     Console.WriteLine("\nArray dapat mempunyai maksimal 20 element.\n");
                 }
                 Console.WriteLine("");
-                Console.WriteLine("----------");
+                Console.WriteLine("------------------------");
+                Console.WriteLine("Masukkan elemen array ");
+                Console.WriteLine("------------------------");
+
+                //Pengguna memasukkan elemen pada array
+                for (int i = 0; i < n ; i++)
+                {
+                    Console.Write("<" + (i + i) + "> ");
+                    string s1 = Console.ReadLine();
+                    a[i] = Int32.Parse(s1);
+                }
+            }
+            public void display()
+            {
+
             }
         }
     }
